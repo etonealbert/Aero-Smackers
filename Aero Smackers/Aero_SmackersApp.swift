15f3a0e25032b760cb847d779cd9cb62e17b2f50
etonealbert <albert.lukmanov99@gmail.com>
2023-06-20 14:24:37 +0400

    base swiftUI

---
+++ Aero Smackers/Aero_SmackersApp.swift
@@ -0,0 +1,17 @@
+//
+//  Aero_SmackersApp.swift
+//  Aero Smackers
+//
+//  Created by Albert on 20.06.2023.
+//
+
+import SwiftUI
+
+@main
+struct Aero_SmackersApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
